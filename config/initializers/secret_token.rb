5c80d8e4d86d63c0065007b7e61d3cd95915be40
Madhavan Rajagopal Padmanabhan <madhavan.rp@npcompete.com>
2013-06-02 14:56:48 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyRailsApp::Application.config.secret_token = '3c342667e2b26d8cf6fb2b56b493febe27f18aee981a8731db8fcb27e8588c59fa4abd0ff43a80f083736030ade63f3e17e192cda62e68f9d52598ef7595cd27'
